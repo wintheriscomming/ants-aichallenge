fd80a38bd16c406dc48d7fe37883980bbf0829b8
Ola Winther <ola.winther@tietoevry.com>
2023-03-20 12:38:01 +0100

    Fixar missat mellanslag i fråga om debugger ska startas

--- TestDotnetBotAgainstPythonHunterBot.bat
+++ TestDotnetBotAgainstPythonHunterBot.bat
@@ -1,3 +1,3 @@
 @echo off
-set /p "launchDebugger=Launch debugger (y or n)?"
+set /p "launchDebugger=Launch debugger (y or n)? "
 python ../tools/playgame.py "dotnet run" "python ../tools/sample_bots/python/HunterBot.py" --map_file ../tools/maps/example/tutorial1.map --log_dir game_logs --turns 60 --scenario --food none --player_seed 7 -e --turntime 3600000 --loadtime 3600000
